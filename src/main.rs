e89750d548b0a087fd5e5f8eafdcebd439c9d083
mihaizaurus <73397939+mihaizaurus@users.noreply.github.com>
2024-12-09 22:44:12 +0100

    Advent of code 2024 day 2

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use std::io;
 mod day1;
+mod day2;
 
 fn main() -> io::Result<()> {
     println!("Hello, advent of code!");
     
-    day1::result()?;
+    // day1::result()?;
+    day2::result()?;
+    
     Ok(())
 }

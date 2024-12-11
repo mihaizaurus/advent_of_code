fe90d8a618f4b48f7ebe6cc048d06b3339048a6a
mihaizaurus <73397939+mihaizaurus@users.noreply.github.com>
2024-12-11 01:34:15 +0100

    Advent of code 2024 day 4 puzzle

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,15 @@ use std::io;
 mod day1;
 mod day2;
 mod day3;
+mod day4;
 
 fn main() -> io::Result<()> {
     println!("Hello, advent of code!");
     
     // day1::result()?;
     // day2::result()?;
-    day3::result()?;
+    // day3::result()?;
+    day4::result()?;
 
     Ok(())
 }

357636d29219162b211524a3fd41546ce75f9181
mihaizaurus <73397939+mihaizaurus@users.noreply.github.com>
2024-12-12 01:22:13 +0100

    Advent of Code day 5 puzzle

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,16 @@ mod day1;
 mod day2;
 mod day3;
 mod day4;
+mod day5;
 
 fn main() -> io::Result<()> {
     println!("Hello, advent of code!");
     
-    // day1::result()?;
-    // day2::result()?;
-    // day3::result()?;
-    day4::result()?;
+    day1::result()?; // Passed
+    day2::result()?; // Passed
+    day3::result()?; // Passed
+    day4::result()?; // Passed
+    day5::result()?;
 
     Ok(())
 }

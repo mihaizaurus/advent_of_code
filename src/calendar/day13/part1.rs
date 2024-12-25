dba7abb1550806e13d10f397487d5dde990119d2
mihaizaurus <73397939+mihaizaurus@users.noreply.github.com>
2024-12-25 17:25:06 +0100

    day 13 part 1 wip

--- src/calendar/day13/part1.rs
+++ src/calendar/day13/part1.rs
@@ -1,6 +1,25 @@
 use crate::common::types::Machine;
 pub fn result(machines: &Vec<Machine>) -> isize {
-    let placeholder = 480;
+    let mut score = 0;
+    
+    for machine in machines {
+        score += get_score(machine);
+    }
+    
+    score
+}
 
-    placeholder
+fn get_score(machine: &Machine) -> isize {
+    // get lowest moves of button_a and button_b to reach prize_location
+    // button a costs 3 tokens per move
+    // button b costs 1 token per move
+    // return the lowest cost
+
+    0
+}
+
+fn get_lowest_moves(machine: &Machine) -> isize {
+    let mut moves = 0;
+
+    moves
 }

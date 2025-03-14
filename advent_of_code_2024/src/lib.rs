e68fdb108cfb2a23f75b9ca1cd76b252e4bc16d8
Mihai <mihaizaurus@protonmail.com>
2025-03-14 13:38:48 +0100

    renamed to puzzle

--- advent_of_code_2024/src/lib.rs
+++ advent_of_code_2024/src/lib.rs
@@ -1,6 +1,6 @@
 use advent_of_code_common::types::PuzzleAnswer;
 
-pub mod calendar {
+pub mod puzzle {
     pub mod day1;
     pub mod day2;
     pub mod day3;
@@ -23,21 +23,21 @@ pub fn run_day(day: usize) -> Result<PuzzleAnswer, String> {
     let output_path = format!("advent_of_code_results/2024/day{}.txt", day); 
 
     match day {
-        1 => calendar::day1::result().map_err(|e| e.to_string()),
-        2 => calendar::day2::result().map_err(|e| e.to_string()),
-        3 => calendar::day3::result().map_err(|e| e.to_string()),
-        4 => calendar::day4::result().map_err(|e| e.to_string()),
-        5 => calendar::day5::result().map_err(|e| e.to_string()),
-        6 => calendar::day6::result().map_err(|e| e.to_string()),
-        7 => calendar::day7::result().map_err(|e| e.to_string()),
-        8 => calendar::day8::result(&input_path, &output_path).map_err(|e| e.to_string()),
-        9 => calendar::day9::result(&input_path, &output_path).map_err(|e| e.to_string()),
-        10 => calendar::day10::result(&input_path, &output_path).map_err(|e| e.to_string()),
-        11 => calendar::day11::result(&input_path, &output_path).map_err(|e| e.to_string()),
-        12 => calendar::day12::result(&input_path, &output_path).map_err(|e| e.to_string()),
-        13 => calendar::day13::result(&input_path, &output_path).map_err(|e| e.to_string()),
-        14 => calendar::day14::result(&input_path, &output_path).map_err(|e| e.to_string()),
-        15 => calendar::day15::result(&input_path, &output_path).map_err(|e| e.to_string()),
+        1 => puzzle::day1::result().map_err(|e| e.to_string()),
+        2 => puzzle::day2::result().map_err(|e| e.to_string()),
+        3 => puzzle::day3::result().map_err(|e| e.to_string()),
+        4 => puzzle::day4::result().map_err(|e| e.to_string()),
+        5 => puzzle::day5::result().map_err(|e| e.to_string()),
+        6 => puzzle::day6::result().map_err(|e| e.to_string()),
+        7 => puzzle::day7::result().map_err(|e| e.to_string()),
+        8 => puzzle::day8::result(&input_path, &output_path).map_err(|e| e.to_string()),
+        9 => puzzle::day9::result(&input_path, &output_path).map_err(|e| e.to_string()),
+        10 => puzzle::day10::result(&input_path, &output_path).map_err(|e| e.to_string()),
+        11 => puzzle::day11::result(&input_path, &output_path).map_err(|e| e.to_string()),
+        12 => puzzle::day12::result(&input_path, &output_path).map_err(|e| e.to_string()),
+        13 => puzzle::day13::result(&input_path, &output_path).map_err(|e| e.to_string()),
+        14 => puzzle::day14::result(&input_path, &output_path).map_err(|e| e.to_string()),
+        15 => puzzle::day15::result(&input_path, &output_path).map_err(|e| e.to_string()),
         _ => Err(format!("Day {} is not implemented or understood...", day)),
     }
 }
